c6315cb58bb1e7c61ffaf93af4df42601cd6c5bc
NguyenHuuNhatQuang <119930189+NguyenHuuNhatQuang@users.noreply.github.com>
2026-02-28 12:15:36 +0700

    Update checker.cpp

--- v4/checker.cpp
+++ v4/checker.cpp
@@ -5,10 +5,6 @@ int main(int argc, char* argv[])
 {
     ifstream inp (argv[1]);
     ifstream ans (argv[2]);
-
-    int cnt = 0, tmp;
-    while (ans >> tmp) cnt ++;
-
-    cout << "cnt = " << cnt;
     return 0;
 }
+
